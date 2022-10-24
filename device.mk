3b91f1478eb67221abe774cfbed81e206afe0afb
Kujou Yuko <i@779.moe>
2022-10-24 17:48:12 +0800

    lisa: Build missing hostapd HIDL interface lib
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: Ic57ccb9041f060c33dd4119f4e42090819304017

--- device.mk
+++ device.mk
@@ -482,6 +482,7 @@ PRODUCT_COPY_FILES += \
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
+    android.hardware.wifi.hostapd@1.0.vendor \
     hostapd \
     libwifi-hal-qcom \
     ipacm \
